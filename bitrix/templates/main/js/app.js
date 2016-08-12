14522b303b0dee018b94a9c30b54ba88c7b53144
kotvasili <kot_vasili@mail.ru>
2016-08-12 17:01:43 +0300

    responsive footer, revorked promo blocks

--- bitrix/templates/main/js/app.js
+++ bitrix/templates/main/js/app.js
@@ -34,6 +34,7 @@ $(function() {
 		accessibility: true,
 		arrows: true,
 		dots: true,
+		draggable: false,
 		appendArrows: $('.galery__nav'),
   nextArrow:'<button type="button" class="carousel-next"><div class="icon"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 9 5" style="enable-background:new 0 0 9 5;" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#9E9E9E;}</style><path class="st0" d="M4.5,5L9,0.6L8.4,0L4.5,3.7L0.6,0L0,0.6L4.5,5"/></svg></div></button>',
   prevArrow:'<button type="button" class="carousel-prev"><div class="icon"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 9 5" style="enable-background:new 0 0 9 5;" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#9E9E9E;}</style><path class="st0" d="M4.5,5L9,0.6L8.4,0L4.5,3.7L0.6,0L0,0.6L4.5,5"/></svg></div></button>',
@@ -42,36 +43,237 @@ $(function() {
 	$('.slick-slider-discount').slick({
 		accessibility: true,
 		arrows: true,
-		slidesToShow: 7,
-		draggable: false
+		slidesToShow: 6,
+		draggable: false,
+		responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 490,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    ]
 	});
-
+	$('.features').slick({
+		accessibility: true,
+		arrows: true,
+		draggable: false,
+		slidesToShow: 4,
+		appendArrows: $('.features__nav'),
+  nextArrow:'<button type="button" class="carousel-next"><div class="icon"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 9 5" style="enable-background:new 0 0 9 5;" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#9E9E9E;}</style><path class="st0" d="M4.5,5L9,0.6L8.4,0L4.5,3.7L0.6,0L0,0.6L4.5,5"/></svg></div></button>',
+  prevArrow:'<button type="button" class="carousel-prev"><div class="icon"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 9 5" style="enable-background:new 0 0 9 5;" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#9E9E9E;}</style><path class="st0" d="M4.5,5L9,0.6L8.4,0L4.5,3.7L0.6,0L0,0.6L4.5,5"/></svg></div></button>',
+		responsive: [
+	    {
+	      breakpoint: 1040,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 578,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+    	]
+	});
 	$('.slick-slider-discount-brand').slick({
 		accessibility: true,
 		arrows: true,
 		slidesToShow: 7,
-		draggable: false
+		draggable: false,
+		responsive: [
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    ]
 	});
 
 	$('.slick-slider-discount-news').slick({
 		accessibility: true,
 		arrows: true,
 		slidesToShow: 3,
-		draggable: false
+		draggable: false,
+		responsive: [
+
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 660,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    ]
 	});
 
 	$('.slick-slider-discount-themed').slick({
 		accessibility: true,
 		arrows: true,
 		slidesToShow: 7,
-		draggable: false
+		draggable: false,
+		responsive: [
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    ]
 	});
 
 	$('.slick-slider-discount-hits').slick({
 		accessibility: true,
 		arrows: true,
 		slidesToShow: 5,
-		draggable: false
+		draggable: false,
+		responsive: [
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 490,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    ]
 	});
 
 	//tabs BRAND
