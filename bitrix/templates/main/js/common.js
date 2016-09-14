c9d01b09096fb6db9c29549a0e54f32692419090
kotvasili <kot_vasili@mail.ru>
2016-09-14 19:23:21 +0300

    fives vol 4 slidIn header on mobile

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -1582,7 +1582,69 @@ if (form_vac.length) {
         });
     });
 };
+// disable hover on scroll
+var body = document.body,
+    timer;
 
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },300);
+}, false);
+
+jQuery(document).ready(function($){
+	var mainHeader = $('#wrap .cd-auto-hide-header'),
+		secondaryNavigation = $('.cd-secondary-nav'),
+		//this applies only if secondary nav is below intro section
+		belowNavHeroContent = $('.sub-nav-hero'),
+		headerHeight = mainHeader.height();
+	
+	//set scrolling variables
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame)
+				? setTimeout(autoHideHeader, 250)
+				: requestAnimationFrame(autoHideHeader);
+		}
+	});
+
+	$(window).on('resize', function(){
+		headerHeight = mainHeader.height();
+	});
+
+	function autoHideHeader() {
+		var currentTop = $(window).scrollTop();
+
+		( belowNavHeroContent.length > 0 ) 
+			? checkStickyNavigation(currentTop) // secondary navigation below intro
+			: checkSimpleNavigation(currentTop);
+
+	   	previousTop = currentTop;
+		scrolling = false;
+	}
+
+	function checkSimpleNavigation(currentTop) {
+		//there's no secondary nav or secondary nav is below primary nav
+	    if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up...
+	    	mainHeader.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down...
+	    	mainHeader.addClass('is-hidden');
+	    }
+	}
+});
 $(".form-col .dropzone").dropzone({ 
 	url: "/file/post",
 	dictDefaultMessage: 'Загрузить фото <span class = "fsize">(не более 5Мб)</span> ',
